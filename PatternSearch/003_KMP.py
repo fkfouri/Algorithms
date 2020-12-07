379aff0d9788a764741b416f736118b6d0d6cc4f
Fabio Kfouri <fabio.kfouri@embraer.net.br>
2020-12-07 00:33:38 -0300

    Correção do codigo KMP

--- PatternSearch/003_KMP.py
+++ PatternSearch/003_KMP.py
@@ -5,17 +5,16 @@
 ###
 # A funcao Falha gasta o tempo de Ɵ(m)
 #
-def FailureFunction(T, P):
+def FailureFunction(P):
     
     j = 0                       # indice que percorre os prefixos
-    i = 0                       # indice que percorre os sufixos
+    i = 1                      # indice que percorre os sufixos
     m = len(P)
-    n = len(T)
 
-    F = [None] * n
+    F = [None] * m
     F[0] = 0
 
-    while i > m:
+    while i < m:
         if P[i] == P[j]:        # ja combinaram j+1 caracteres
             j += 1
             F[i] = j
@@ -30,18 +29,24 @@ def FailureFunction(T, P):
     return F
 
 
+def printComparacoes(numero, text):
+    print(numero, 'é o numero de comparacoes em:', '"' + text +'"')
 
 def KMPMatch(T, P):
-    F = FailureFunction(T, P)              #gasta tempo Ɵ(m)
+    F = FailureFunction(P)              #gasta tempo Ɵ(m)
 
     i = 0
     j = 0
     m = len(P)
     n = len(T)
 
+    comparacoes = 0
+
     while (i < n):
+        comparacoes +=1
         if (T[i] == P[j]):
             if j == m-1:
+                printComparacoes(comparacoes, T)
                 return i - j
             else:
                 i += 1
@@ -52,12 +57,35 @@ def KMPMatch(T, P):
             else:
                 i += 1
     
+    printComparacoes(comparacoes, T)
     return -1
 
 
 
 texto = "This is a simple text for a test."
 palavra = "text"
+print(KMPMatch(texto, palavra))
+
+texto = "vi na mata uma arara e duas aranhas"
+palavra = "araras"
+print(KMPMatch(texto, palavra))
 
 
+texto = "faturei com folga na prova de complexidade de algoritmo"
+palavra = "algo"
+print(KMPMatch(texto, palavra))
+
+
+texto = "abacaabaccabacabaabb"
+palavra = "abacab"
+print(KMPMatch(texto, palavra))
+
+
+texto = "a jabuticaba acabou de acabar"
+palavra = "cabar"
+print(KMPMatch(texto, palavra))
+
+
+texto = "esta prova e mais longa que complicada"
+palavra = "cada"
 print(KMPMatch(texto, palavra))
