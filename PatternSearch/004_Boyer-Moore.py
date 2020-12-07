880a07ac8a882c50f519d0a226f7bb693d402d57
Fabio Kfouri <fabio.kfouri@embraer.net.br>
2020-12-07 07:24:15 -0300

    Tests de Boyer Mooer

--- PatternSearch/004_Boyer-Moore.py
+++ PatternSearch/004_Boyer-Moore.py
@@ -1,6 +1,7 @@
 
 
-
+def printComparacoes(numero, text):
+    print(numero, 'é o numero de comparacoes em:', '"' + text +'"')
 
 ###
 # A funcao Falha gasta o tempo de Ɵ(m)
@@ -21,7 +22,7 @@ def UltimaOcorrencia(P):
     return L
 
 
-def BoyerMooreMathc(T, P):
+def BoyerMooreMatch(T, P):
 
     L = UltimaOcorrencia(P)
 
@@ -30,9 +31,13 @@ def BoyerMooreMathc(T, P):
     i = m-1
     j = m-1
 
+    comparacoes = 0
+
     while True:
+        comparacoes+=1
         if T[i] == P[j]:
             if j == 0:
+                printComparacoes(comparacoes, T)
                 return i
             else:
                 i -= 1
@@ -46,12 +51,30 @@ def BoyerMooreMathc(T, P):
         if i > n - 1:
             break
     
+    printComparacoes(comparacoes, T)
     return -1
 
 
 
 texto = "This is a simple text for a test."
 palavra = "text"
+print(BoyerMooreMatch(texto, palavra))
+
+texto = "a pattern matching algorithm"
+palavra = "rithm"
+print(BoyerMooreMatch(texto, palavra))
 
 
-print(BoyerMooreMathc(texto, palavra))
+texto = "abacaabadcabacabaabb"
+palavra = "abacab"
+print(BoyerMooreMatch(texto, palavra))
+
+
+texto = "a jabuticaba acabou de acabar"
+palavra = "cabar"
+print(BoyerMooreMatch(texto, palavra))
+
+
+texto = "vi na mata uma arara e duas aranhas"
+palavra = "araras"
+print(BoyerMooreMatch(texto, palavra))
